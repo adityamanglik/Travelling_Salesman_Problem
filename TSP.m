5fbffdf323b2a132086c8f168bf32898da933922
adityamanglik <aditya.account@outlook.com>
2016-02-22 23:46:46 +0530

    Two solution implemeted

--- TSP.m
+++ TSP.m
@@ -2,11 +2,12 @@
 clear;
 clc;
 %%Initial Declarations
-num_city=10;    %Set number of cities
+num_city=8;    %Set number of cities
 max_dist=100;  %Set maximum distance spread
 route=zeros(1,num_city);
 %%Random generation of City Locations
 loc_city=randi([1 max_dist],num_city,2);
+display('Cities generated.');
 %Calculating Distances between cities
     distances=zeros(num_city,num_city);
  for i=1:num_city
@@ -14,14 +15,14 @@ loc_city=randi([1 max_dist],num_city,2);
      distances(i,j)=sqrt((loc_city(j,1)-loc_city(i,1))^2 + (loc_city(j,2)-loc_city(i,2))^2);
      end
  end
- 
+ display('Inter city distances calculated.'); 
  distances=distances+distances'
+ backup_distances=distances;
  %Setting same city distances to max for ease of calculation of route
+ display('Setting same city distances to max for ease of calculation of route.');
  for i=1:num_city
      distances(i,i)=max_dist*1.414+1;
  end
- route(1)=1;
- 
  
  %%Making route using combinations solution
  %REDUCING NUMBER OF CITIES TO MAKE CALCULATIONS FEASIBLE
@@ -36,16 +37,19 @@ loc_city=randi([1 max_dist],num_city,2);
      end
  end
  best_sol=1;
- for i=2:num
+ for i=2:num_rows
      if(dataset(i)<dataset(best_sol))
          best_sol=i;
      end
  end
-     p(best_sol,:)
+ display('Perfect solution after evaluation of all permutations is:');
+ true_solution=p(best_sol,:)
+ display('Perfect solution requires a distance of:');
+ true_solution_dist=dataset(best_sol)
      
      
  %%Making route using Nearest neighbour approach
- route(1)=1;
+ route(1)=randi([1 num_city],1,1);
  for i=2:num_city
      min=1;
      for j=2:num_city
@@ -56,9 +60,16 @@ loc_city=randi([1 max_dist],num_city,2);
      route(i)=min;
      distances(:,route(i-1))=max_dist*1.414+1;
  end
- 
- %%Displaying Values 
+ display('Greedy route is:');
      route
+ greedy_distance=0;
+     for j=2:num_city
+     greedy_distance=greedy_distance + backup_distances(route(j),route(j-1));
+     end
+ %%Displaying Values 
+
+ display('Greedy route requires a distance of:');
+     greedy_distance    
  
 %  distances
 %  for i=1:num_city
